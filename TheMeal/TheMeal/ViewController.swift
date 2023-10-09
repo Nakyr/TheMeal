5ff318286ca6245c3f0e29bec0ccc6dc1dbd8d57
Nakyr <33633346+Nakyr@users.noreply.github.com>
2023-10-08 18:56:25 -0600

    Starting project

---
+++ TheMeal/TheMeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMeal
+//
+//  Created by Armando Corona Carrillo on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
